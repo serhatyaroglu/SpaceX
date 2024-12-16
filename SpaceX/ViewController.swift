bc808055c8fcd6bf397101c3c0997a8345c09587
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2024-12-16 20:17:57 +0300

    Initial Commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by serhat yaroglu on 16.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpaceXTests/SpaceXTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXTests.swift
+//  SpaceXTests
+//
+//  Created by serhat yaroglu on 16.12.2024.
+//
+
+import Testing
+@testable import SpaceX
+
+struct SpaceXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
